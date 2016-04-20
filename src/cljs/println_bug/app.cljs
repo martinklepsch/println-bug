3bbfb18f98142489320bd786dce0521c99203df2
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-20 17:54:32 +0200

    init

---
+++ src/cljs/println_bug/app.cljs
@@ -0,0 +1,10 @@
+(ns println-bug.app)
+
+(enable-console-print!)
+
+(println "test")
+
+(defn init []
+  (let [c (.. js/document (createElement "DIV"))]
+    (aset c "innerHTML" "<p>i'm dynamically created</p>")
+    (.. js/document (getElementById "container") (appendChild c))))
